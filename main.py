174e8a41ddf2c3d1d2388508016d04e2e480593c
Night1918 <admin@night1918.top>
2023-12-25 09:06:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -120,7 +120,8 @@ booking_seat_data={
     'segment': booking_date_['data'][0]['times'][0]['id'],
     'day': booking_date_['data'][0]['day'],
     'startTime': booking_date_['data'][0]['times'][0]['start'],
-    'endTime': booking_date_['data'][0]['times'][0]['end'],
+    # 'endTime': booking_date_['data'][0]['times'][0]['end'],
+    'endTime': "22:30",
     'authorization': booking_user_authorization,
 }
 booking_seat_resp=session.post(booking_seat_url,headers=booking_seat_headers,data=booking_seat_data)
